7f7ae905cdb2c30ae42cc0fea4cfd3fc34e1a5ab
DeboDevelop <debajyotidofficial@gmail.com>
2021-02-11 19:09:20 +0530

    Added react router and home, login and loading pages in frontend. Also got the basic server for backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/pages/Home";
+import Loading from "./Components/pages/Loading";
+import Login from "./Components/pages/Login";
+
 function App() {
     return (
         <div>
-            <h1>Hello World!!!</h1>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/github/callback">
+                    <Loading />
+                </Route>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+            </Switch>
         </div>
     );
 }
