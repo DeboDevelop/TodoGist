2efdb3359e4a4526960b18225035afef150b936e
DeboDevelop <debajyotidofficial@gmail.com>
2021-02-15 18:02:33 +0530

    Created the routes for projects.

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -2,21 +2,53 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../models/projects");
 const { getUser } = require("../middleware/common");
+const { getProject } = require("../middleware/projects");
 
-router.get("/", getUser, (req, res) => {
-    res.status(200).json({ user: res.user });
+// Getting All
+router.get("/", getUser, async (req, res) => {
+    let projects;
+    try {
+        projects = await Project.find({ user: res.user });
+        res.status(200).json(projects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 // Getting One
-router.get("/:id", (req, res) => {});
+router.get("/:id", getProject, (req, res) => {
+    res.json(res.project);
+});
 
 // Creating one
-router.post("/", async (req, res) => {});
+router.post("/", getUser, async (req, res) => {
+    if (req.body.title === "" || req.body.title === undefined || req.body.title === null) {
+        return res.status(400).json({ err: "No title is given" });
+    }
+    const project = new Project({
+        user: res.user,
+        title: req.body.title,
+    });
+    try {
+        const newProject = await project.save();
+        return res.status(201).json(newProject);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+});
 
 // Updating One
-router.patch("/:id", async (req, res) => {});
-
-// Deleting One
-router.delete("/:id", async (req, res) => {});
+router.patch("/:id", getProject, async (req, res) => {
+    if (req.body.title === "" || req.body.title === undefined || req.body.title === null) {
+        return res.status(400).json({ err: "No title is given" });
+    }
+    res.project.title = req.body.title;
+    try {
+        const updatedProject = await res.project.save();
+        return res.json(updatedProject);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+});
 
 module.exports = router;
